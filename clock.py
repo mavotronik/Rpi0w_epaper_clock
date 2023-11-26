e5f6a0e8b1a37a795dd11a8d2b95dbfd3b560c76
Matvey <66329687+mavotronik@users.noreply.github.com>
2023-11-26 19:05:12 +0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -15,30 +15,41 @@ FONT = os.path.join(RES_DIR, 'FreeMono.ttf')
 FONTBOLD = os.path.join(RES_DIR, 'FreeMonoBold.ttf')
 
 timefont_size = 35
-
 timefont = ImageFont.truetype(FONTBOLD, timefont_size)
+
+NumToClear = 0
 
 try:
 
     epd = epd2in13_V3.EPD()
     epd.init()
-    #epd.Clear(0xFF)
     
-    dt_now = datetime.datetime.now()
-    time_rn = dt_now.strftime(TIMEFORMAT)
-    date_rn = dt_now.strftime(DATEFORMAT)
+    
+    while True:
+        epd.init()
+        
+        if (NumToClear == 5):
+            epd.Clear(0xFF)
+            NumToClear = 0  
+    
+        dt_now = datetime.datetime.now()
+        time_rn = dt_now.strftime(TIMEFORMAT)
+        date_rn = dt_now.strftime(DATEFORMAT)
 
-    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(image)
-    # draw = draw.transpose(Image.ROTATE_180)
-    draw.text((40, 15), time_rn, font = timefont, fill = 0)
-    draw.text((20, 60), date_rn, font = timefont, fill = 0)
-    epd.displayPartial(epd.getbuffer(image))
+        image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(image)
+        # draw = draw.transpose(Image.ROTATE_180)
+        draw.text((40, 15), time_rn, font = timefont, fill = 0)
+        draw.text((20, 60), date_rn, font = timefont, fill = 0)
+        epd.displayPartial(epd.getbuffer(image))
     
-    time.sleep(0.5)
-    epd.Clear(0xFF)
-    epd.sleep()
-   
+        epd.Clear(0xFF)
+        epd.sleep()
+        NumToClear = NumToClear + 1
+        print(f"NumToClear:", NumToClear)
+        
+        time.sleep(10)
+
 
 except IOError as e:
     print(e)
