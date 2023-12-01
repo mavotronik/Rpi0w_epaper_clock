dd21ef23d4df917fc817c7d54770b99d9ecc68a2
Matvey <66329687+mavotronik@users.noreply.github.com>
2023-12-01 15:56:30 +0300

    Add home-assistant data

--- clock.py
+++ clock.py
@@ -7,14 +7,18 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import psutil
+import requests
 
 RES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources/')    # directory with fonts, pictures, etc...
 
 DATEFORMAT = "%d/%m/%y"
-TIMEFORMAT = "%H:%M:%S"
+TIMEFORMAT = "%H:%M"
 
 FONT = os.path.join(RES_DIR, 'FreeMono.ttf')
 FONTBOLD = os.path.join(RES_DIR, 'FreeMonoBold.ttf')    # fonts
+
+HA_IP = "your homeassistant ip:8123"
+HA_TOKEN = "your long live homeassistant token"
 
 timefont_size = 35
 timefont = ImageFont.truetype(FONTBOLD, timefont_size)    # font for display time
@@ -27,6 +31,14 @@ resfont = ImageFont.truetype(FONT, resfont_size)    # font for display system re
 
 NumToClear = 0
 
+
+
+url = f"http://{HA_IP}/api/states/sensor.datchik_za_oknom_temperature"
+headers = {
+    "Authorization": f"Bearer {HA_TOKEN}",
+    "content-type": "application/json",
+}
+
 try:
 
     epd = epd2in13_V3.EPD()
@@ -36,9 +48,17 @@ try:
     while True:
         epd.init()    # initial display again after sleeping
         
-        if (NumToClear == 10):    # numbers of display partial updates to do full update
+        if (NumToClear == 30):    # numbers of display partial updates to do full update
             epd.Clear(0xFF)
             NumToClear = 0  
+    
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            out_temp_value = data['state']
+            #print("State value:", state_value)
+        else:
+            print("Failed to fetch data:", response.status_code)
     
         dt_now = datetime.datetime.now()
         seconds_until_next_minute = 60 - dt_now.time().second    # calculate secons until next minute
@@ -48,7 +68,8 @@ try:
         cpu_ = psutil.cpu_percent()
         mem_ = psutil.virtual_memory()
         cpu = "CPU:" + str(cpu_) + "%"
-        mem = "RAM:" + str(mem_.percent)+ "%"    # create string to send to display 
+        mem = "RAM:" + str(mem_.percent)+ "%"     
+        out_temp_value_str = "Ул. температура: "+out_temp_value+" °C"    # create string to send to display
 
         image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
         draw = ImageDraw.Draw(image)
@@ -56,8 +77,9 @@ try:
         draw.line([(0,105),(250,105)], fill = 0,width = 3)    # draw lower line
         
 
-        draw.text((25, 25), time_rn, font = timefont, fill = 0)    # display time
+        draw.text((60, 25), time_rn, font = timefont, fill = 0)    # display time
         draw.text((75, 65), date_rn, font = datefont, fill = 0)    # display date
+        draw.text((10, 80), out_temp_value_str, font = resfont, fill = 0)    # display outdoor temp
         
         draw.text((55, 3), "192.168.2.167", font = resfont, fill = 0)    # display text
         draw.text((5, 111), cpu, font = resfont, fill = 0)    # display cpu usage
